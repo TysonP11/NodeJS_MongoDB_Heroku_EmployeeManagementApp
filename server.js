4d7e52831e13370712a85d3e68f8def41a975a67
TysonP11 <59603295+TysonP11@users.noreply.github.com>
2020-02-28 01:47:26 +0700

    changed Port to deploy on heroku

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const employeeController = require('./controllers/EmployeeController');
 const storeController = require('./controllers/StoreController');
 
 var app = express();
+var PORT = 3000 || process.env.PORT;
 
 app.use(bodyparser.urlencoded({
     extended: true
@@ -19,7 +20,7 @@ app.set('views', path.join(__dirname, '/views'));
 app.engine('hbs', exphbs( {extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
 app.set('view engine', 'hbs');
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('Express server started at port: 3000');
 });
 
